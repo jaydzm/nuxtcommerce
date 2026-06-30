a805d5b6094ef0a1c8aab0e9b81d8762894027f4
jaydzm <jaydzm@163.com>
2026-06-30 22:15:19 +0800

    Update AppFooter.vue

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -27,7 +27,7 @@ const chooseLocale = code => {
   <footer class="my-5 flex items-center justify-between gap-3 px-5 text-[13px] font-semibold text-secondary-text dark:text-secondary-text-d">
     <div class="truncate">
       <a class="transition-all hover:text-black hover:dark:text-neutral-100" href="https://github.com/zackha/nuxtcommerce" target="_blank">
-        NuxtCommerce v{{ config.public.version }}
+        支持NuxtCommerce v{{ config.public.version }}
       </a>
       —
       {{ $t('footer.developed_by_author') }}
