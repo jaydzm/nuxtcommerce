8d0263330d585a49de2214be0ed5837848ca7832
jaydzm <jaydzm@163.com>
2026-06-30 20:16:44 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,7 @@
 // app/app.config.ts
 export default defineAppConfig({
   site: {
-    name: 'NuxtCommerce',
+    name: '口感快车',
     description: 'NuxtCommerce is a dynamic e-commerce solution developed with Nuxt 4 and GraphQL, tailored for WooCommerce.',
   },
   ui: {
