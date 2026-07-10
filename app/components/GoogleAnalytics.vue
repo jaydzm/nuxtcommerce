478e30759f31057b6252a4889d0cf1a8a6f3d887
jaydzm <jaydzm@163.com>
2026-07-10 17:25:30 +0800

    Create GoogleAnalytics.vue

---
+++ app/components/GoogleAnalytics.vue
@@ -0,0 +1,18 @@
+<!-- components/GoogleAnalytics.vue -->
+<template>
+  <ClientOnly>
+    <div style="display: none;">
+      <script async :src="`https://www.googletagmanager.com/gtag/js?id=${gaId}`"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', '{{ gaId }}');
+      </script>
+    </div>
+  </ClientOnly>
+</template>
+
+<script setup>
+const gaId = 'G-Q6SH4F3C8Q' // 👈 换成你自己的
+</script>
