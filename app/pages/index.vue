d16a8cc034d1af5a4278b4a47a4593aa328f2c97
jaydzm <jaydzm@163.com>
2026-07-17 00:45:25 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <!-- app/pages/index.vue -->
 <template>
-  <div class="container mx-auto px-4 py-8">
+  <div class="mx-auto px-4 py-8">
     <h1 class="text-2xl font-bold mb-6">全部产品</h1>
     <ProductList />
   </div>
