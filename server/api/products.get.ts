1cbb2f24ce1215a9172565ba4a730451d0e5f57e
jaydzm <jaydzm@163.com>
2026-07-17 03:46:09 +0800

    Update products.get.ts

--- server/api/products.get.ts
+++ server/api/products.get.ts
@@ -8,7 +8,7 @@ export default cachedEventHandler(
     return await requestQuery(getProductsQuery, { after, search, category, order, field });
   },
   {
-    maxAge: 5,
+    maxAge: 0,
     swr: true,
     getKey: event => event.req.url!,
   }
