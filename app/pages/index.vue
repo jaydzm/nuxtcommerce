d8dd162b45ca1fdc0ad75ece7c3a1462c8762534
jaydzm <jaydzm@163.com>
2026-07-17 00:52:10 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <!-- app/pages/index.vue -->
 <template>
   <div class="mx-auto px-4 py-8">
-    <h1 class="text-2xl font-bold mb-6">全部产品</h1>
+    <h1 class="text-2xl font-bold mb-6">轻松找好茶，全免费试喝</h1>
     <ProductList />
   </div>
 </template>
