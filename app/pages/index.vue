d1f73f76cd22a3d75e4ea3998c9e96f8b2a87ab8
jaydzm <jaydzm@163.com>
2026-07-19 22:34:24 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,5 @@
 <!-- app/pages/index.vue -->
 <template>
-  <div class="entry-link">
-      <NuxtLink to="/swipe">🎬 沉浸式浏览</NuxtLink>
-    </div>
   <div class="mx-auto px-3">
     <ProductList />
   </div>
