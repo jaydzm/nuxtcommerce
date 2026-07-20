0f5fdfb7e9b5b94b00a8a11c061b845dbf9d43d8
jaydzm <jaydzm@163.com>
2026-07-20 19:04:36 +0800

    Update product.get.ts

--- server/api/product.get.ts
+++ server/api/product.get.ts
@@ -1,11 +1,52 @@
 // server/api/product.get.ts
-import { getProductQuery } from '~/gql/queries/getProduct';
 import { requestQuery } from '~~/server/utils/wpgraphql';
+
+// 单一商品的 GraphQL 查询语句
+const getProductQuery = /* GraphQL */ `
+  query getProduct($slug: ID!) {
+    product(id: $slug, idType: SLUG) {
+      databaseId
+      name
+      slug
+      description
+      shortDescription
+      image {
+        sourceUrl
+      }
+      galleryImages {
+        nodes {
+          sourceUrl
+        }
+      }
+      categories {
+        nodes {
+          name
+          slug
+        }
+      }
+      ... on SimpleProduct {
+        price
+        regularPrice
+        salePrice
+        stockQuantity
+        stockStatus
+      }
+    }
+  }
+`;
 
 export default cachedEventHandler(
   async event => {
-    const { slug, sku } = getQuery(event) as { slug?: string; sku?: string };
-    return await requestQuery(getProductQuery, { slug, sku });
+    const { slug } = getQuery(event) as { slug?: string; sku?: string };
+
+    if (!slug) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Missing product slug',
+      });
+    }
+
+    return await requestQuery(getProductQuery, { slug });
   },
   {
     maxAge: 0,
