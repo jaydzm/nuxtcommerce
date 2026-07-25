337968f2ede61cf9dea5bee3502ee83b43086e9e
jaydzm <jaydzm@163.com>
2026-07-25 19:06:12 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,6 +54,7 @@ export default defineNuxtConfig({
     "/": { prerender: true },
     "/categories": { swr: 10000 },
     "/favorites": { swr: 6000 },
+    "/product": { swr: 30 },
   },
 
   nitro: {
